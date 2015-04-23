3b3390db91a286d9738a50186710a33b4a583bce
calf <calfjohn@gmail.com>
2015-04-23 11:57:11 +0800

    Support download file from ftp server

--- cocos_upgrade.py
+++ cocos_upgrade.py
@@ -10,62 +10,35 @@
 import os
 import sys
 import cocos
-import excopy
 import subprocess
 import modify_file
-import shutil
+import download_files
 from argparse import ArgumentParser
 
 UPGRADE_PATH = 'Upgrade'
+PATCH_FILE_NAME = '0001-Upgrade.patch'
+FILES_SERVER_URL = 'http://cocos.qudao.info'
+version_files = ['cocos2d/cocos/cocos2d.cpp',
+                 'cocos2d/cocos/2d/cocos2d.cpp',
+                 'frameworks/cocos2d-x/cocos/cocos2d.cpp',
+                 'frameworks/cocos2d-x/cocos/2d/cocos2d.cpp',
+                 'frameworks/js-bindings/bindings/manual/ScriptingCore.h']
 
-#'3.0', '3.1.1', '3.2', '3.3',
-patchList = ['3.3rc2', '3.4', '3.5']
-# patchMap = {
-#             '3.4': '0005-35.patch',
-#             '3.3': '0004-34.patch',
-#             '3.2': '0003-33.patch',
-#             '3.1.1': '0002-32.patch',
-#             '3.0': '0001-311.patch'
-#             }
+def get_project_version(path, project_type):
+    file_path = None
+    for file in version_files:
+        file_path = os.path.join(path, file)
+        if os.path.exists(file_path):
+            break
 
-patchMap = {
-            '3.5': '2lua35-36.diff',
-            '3.4': '1lua34-35.diff',
-            '3.3rc2': '0lua33rc2-34.diff',
-            '3.3': '0004-34.patch',
-            '3.2': '0003-33.patch',
-            '3.1.1': '0002-32.patch',
-            '3.0': '0001-311.patch'
-            }
-
-def os_is_win32():
-    return sys.platform == 'win32'
-
-
-def os_is_mac():
-    return sys.platform == 'darwin'
-
-def get_project_version(proj_path):
-    file_path = os.path.join(proj_path, 'cocos2d/cocos/2d/cocos2d.cpp')
-    if not os.path.exists(file_path):
-        file_path = os.path.join(proj_path, 'cocos2d/cocos/cocos2d.cpp')
-        if not os.path.exists(file_path):
-            file_path = os.path.join(proj_path, 'frameworks/cocos2d-x/cocos/2d/cocos2d.cpp')
-            if not os.path.exists(file_path):
-                file_path = os.path.join(proj_path, 'frameworks/cocos2d-x/cocos/cocos2d.cpp')
-                if not os.path.exists(file_path):
-                    return None
+    if file_path is None:
+        sys.exit(1)
 
     file_modifier = modify_file.FileModifier(file_path)
-    version = file_modifier.findEngineVesion()
-    return get_key_by_version(version)
-
-
-def get_key_by_version(version):
-    for key in patchMap:
-        result = version.find(key)
-        if result > -1:
-            return key
+    if project_type == 'js':
+        return file_modifier.findEngineVesion(r".*#define[ \t]+ENGINE_VERSION[ \t]+\"(.*)\"")
+    elif project_type == 'lua' or project_type == 'cpp':
+        return file_modifier.findEngineVesion(r".*return[ \t]+\"(.*)\";")
 
     return None
 
@@ -86,52 +59,24 @@ if __name__ == '__main__':
 
     print('Receive arguments target:%s name:%s version:%s' % (args.projPath, args.projName, args.upgradeVersion))
 
-    target_project_path = args.projPath + UPGRADE_PATH
-    if not os.path.exists(target_project_path):
-        cocos.Logging.info("> Copy your project into %s ..." % target_project_path)
-        excopy.copy_directory(args.projPath, target_project_path)
+    project_type = cocos.check_project_type(args.projPath)
+    if project_type is None:
+        cocos.Logging.error("> This is not a cocos2d project.")
+        sys.exit(1)
+    project_version = get_project_version(args.projPath, project_type)
+    project_version = project_version.split(' ')[1]
 
-    # Creat a git repository if there is not a repository
-    cmd = "git init \n git add -A \n git commit -m \'Init project for cocos upgrade.\'"
-    ret = subprocess.call(cmd, cwd=target_project_path, shell=True)
-    # if ret != 0:
-    #     sys.exit(1)
+    patch_file_url = str.format('%s/ftp/%s/%s_%s/%s' % (FILES_SERVER_URL, 'cpp', '3.4', '3.5', PATCH_FILE_NAME))
+    # patch_file_url = str.format('%s/%s/%s_%s/%s' % (FILES_SERVER_URL, project_type, project_version, args.upgradeVersion, PATCH_FILE_NAME))
+    patch_file_path = os.path.join(os.getcwd(), PATCH_FILE_NAME)
+    if not os.path.exists(patch_file_path):
+        installer = download_files.CocosZipInstaller(patch_file_url, patch_file_path)
+        installer.download_file()
 
-    patch_path = str.format("%s/patch" % target_project_path)
-    if os.path.exists(patch_path):
-        shutil.rmtree(patch_path)
-    os.mkdir(patch_path)
+    cmd = str.format('python cocos_upgrade2.py -d %s -n %s -p %s ' %
+                     (args.projPath, args.projName, patch_file_path))
+    ret = subprocess.call(cmd, cwd=os.getcwd(), shell=True)
+    if ret != 0:
+        sys.exit(1)
 
-    diff_path = str.format("%s/diff" % target_project_path)
-    if os.path.exists(diff_path):
-        shutil.rmtree(diff_path)
-    os.mkdir(diff_path)
-
-    temp_find = False
-    patch_tag = get_project_version(target_project_path)
-    for i in patchList:
-        if patch_tag == i:
-            temp_find = True
-        if args.upgradeVersion == i:
-            break
-        if temp_find:
-            patch_file = os.path.join(os.getcwd(), 'patch', patchMap[i])
-            shutil.copy(patch_file, patch_path)
-
-    for roots, dirs, files in os.walk(patch_path):
-        for f in files:
-            cmd = str.format('sed -e \'s/HelloLua/%s/g\' %s > %s'
-                             % (args.projName, os.path.join(roots, f), os.path.join(diff_path, f)))
-            ret = subprocess.call(cmd, cwd=patch_path, shell=True)
-            if ret != 0:
-                sys.exit(1)
-
-    for roots, dirs, files in os.walk(diff_path):
-        for f in files:
-            cmd = str.format("git apply --reject -p 1 %s" % os.path.join(roots, f))
-            ret = subprocess.call(cmd, cwd=target_project_path, shell=True)
-
-            cmd = str.format("git add -A \n git commit -m \'%s\'" % f)
-            ret = subprocess.call(cmd, cwd=target_project_path, shell=True)
-            if ret != 0:
-                sys.exit(1)
+    os.remove(patch_file_path)
